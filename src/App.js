8143ef5f7b3f4c8f8e19eaf69fd724e49220e73d
David Esposto <desposto@uwo.ca>
2022-01-14 17:31:03 -0500

    Add Loading Screen when fetching data from API

--- src/App.js
+++ src/App.js
@@ -1,40 +1,44 @@
-import './App.css';
-import React, {useState, useEffect} from 'react';
-import ImageBox from './components/ImageBox'
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import ImageBox from "./components/ImageBox";
 
 var today = new Date();
-var day = String(today.getDate()).padStart(2, '0');
-var month = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var day = String(today.getDate()).padStart(2, "0");
+var month = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
 var year = today.getFullYear();
-var defaultStartDate = '2022-' + '01' + '-' + '03';
-var defualtEndDate = year + '-' + month + '-' + day;
+var defaultStartDate = "2022-" + "01" + "-" + "03";
+var defualtEndDate = year + "-" + month + "-" + day;
 
 function App() {
-  
   const [images, setImages] = useState([]);
-  
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
-    fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_NASA_API_KEY}&start_date=${defaultStartDate}&end_date=${defualtEndDate}`)
-    .then(res => res.json())
-    .then(data => {
-      console.log(data)
-      setImages(data);
-    })
-    .catch(err => console.log(err));
+    fetch(
+      `https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_NASA_API_KEY}&start_date=${defaultStartDate}&end_date=${defualtEndDate}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setImages(data);
+      })
+      .catch((err) => console.log(err));
   }, []);
 
   return (
-    <div className="container mx-auto cursor-pointer"> 
-      <div className="text-2xl flex justify-center"> 
-        <div className="grid grid-cols-1 bg-gray-100"> 
-          <div className='font-bold ml-2'>
-            Spacetagram
+    <div className="container mx-auto cursor-pointer">
+      {isLoading ? (
+        <h1 className="text-5xl text-center mx-auto mt-5">Loading from API</h1>
+      ) : (
+        <div className="text-2xl flex justify-center">
+          <div className="grid grid-cols-1 bg-gray-100">
+            <div className="font-bold ml-2">Spacetagram</div>
+            {images.map((image, index) => (
+              <ImageBox key={index} image={image} />
+            ))}
           </div>
-          {images.map((image, index) =>(
-            <ImageBox key = {index} image={image}/>
-          ))}
         </div>
-      </div>
+      )}
     </div>
   );
 }
