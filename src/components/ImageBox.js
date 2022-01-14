1d809d1e7952ab9cfb302a0dd924029ed853a9b6
David Esposto <desposto@uwo.ca>
2022-01-14 16:05:44 -0500

    Implement Like Button

--- src/components/ImageBox.js
+++ src/components/ImageBox.js
@@ -1,18 +1,24 @@
 import React from 'react'
+import LikeButton from './LikeButton';
 
 const ImageBox = ({image}) => {
     return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg my-2 mx-2 bg-white">
-        <div className="px-3 py-2 text-xl font-bold">
+        <div className='flex pt-2'>
+        <div className="px-2 text-xl font-bold">
             {image.title}
         </div>
-        <img src={image.url} alt ="" className ="w-full"/>
-        <div className="px-6 py-4">
-          <div className=" text-black text-sm ">
-           {image.date} 
-          </div>
+        <div className="px-2 text-sm">
+            {image.date}
         </div>
-        <div className="px-6 py-0.5 mb-2">
+        </div>
+        <img src={image.url} alt ="" className ="w-full"/>
+        <div className="px-4 py-2">
+        <div>
+            <LikeButton/>
+        </div>
+        </div>
+        <div className="px-4 py-0.5 mb-2">
           <div className=" text-black text-sm">
              {image.explanation}
           </div>
