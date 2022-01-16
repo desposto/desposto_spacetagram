9ee1ded11866ea24bb4c5558587f43cc8a369edc
David Esposto <desposto@uwo.ca>
2022-01-16 18:53:32 -0500

    Small Styling Adjustments

--- src/components/APILoadingScreen.js
+++ src/components/APILoadingScreen.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { FaSpinner } from "react-icons/fa";
 
 const APILoadingScreen = () => {
   return (
-    <div className="font-bold text-center mx-auto mt-5 grid-rows-3">
+    <div className="font-bold flex justify-center text-center mx-auto mt-5 flex-col">
       <div className="text-6xl">Spacetagram </div>
-      <div className="text-4xl mt-3">
+      <div className="text-4xl mt-3 py-3">
         Brought to you by NASA Picture of the Day API
+      </div>
+      <div className="text-7xl font-bold w-full flex justify-center pt-5">
+        <FaSpinner className="animate-spin" />
       </div>
     </div>
   );
