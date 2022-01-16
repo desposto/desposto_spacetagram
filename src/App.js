bb538c5fc1531c79e7981f6bdba774ad1d6b516a
David Esposto <desposto@uwo.ca>
2022-01-16 16:42:49 -0500

    Added Loading screen when fetching data from API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
 import ImageBox from "./components/ImageBox";
+import APILoadingScreen from "./components/APILoadingScreen";
 
 var today = new Date();
 var day = String(today.getDate()).padStart(2, "0");
@@ -11,6 +12,7 @@ var defualtEndDate = year + "-" + month + "-" + day;
 
 function App() {
   const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetch(
@@ -20,20 +22,28 @@ function App() {
       .then((data) => {
         console.log(data);
         setImages(data);
+        setIsLoading(false);
       })
       .catch((err) => console.log(err));
   }, []);
 
   return (
     <div className="container mx-auto cursor-pointer">
-      <div className="text-2xl flex justify-center">
-        <div className="grid grid-cols-1 bg-gray-100">
-          <div className="font-bold ml-3">Spacetagram</div>
-          {images.map((image, index) => (
-            <ImageBox key={index} image={image} index={index} />
-          ))}
+      {isLoading ? (
+        <APILoadingScreen />
+      ) : (
+        <div className="text-2xl flex justify-center">
+          <div className="grid grid-cols-1 bg-gray-100">
+            <div className="ml-3 grid-rows-2">
+              <div className="font-bold">Spacetagram</div>
+              <div className="text-sm">Brought to you by NASA Picture of the Day API</div>
+            </div>
+            {images.map((image, index) => (
+              <ImageBox key={index} image={image} index={index} />
+            ))}
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
